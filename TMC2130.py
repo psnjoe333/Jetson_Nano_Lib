d298e587be11ad6e85082ac76193d10607f844e7
psnjoe333 <liwanjoe@gmail.com>
2021-04-03 03:33:20 +0800

    Version 2 - can run the motor but can read the status

--- TMC2130.py
+++ TMC2130.py
@@ -1,6 +1,7 @@
 import spidev
 import Jetson.GPIO as GPIO
 import numpy as np
+import time
 
 class TMC2130():
 
@@ -31,10 +32,11 @@ class TMC2130():
         self.spi.mode = 0b11
         self.spi.lsbfirst = False
 
+
         #Setup GPIO configuration
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(self.En_Pin, GPIO.OUT, initial=GPIO.HIGH)
+        GPIO.setup(self.En_Pin, GPIO.OUT, initial=GPIO.HIGH) #LOW -> active
         GPIO.setup(self.Dir_Pin, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.Step_Pin, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.CS_Pin, GPIO.OUT, initial=GPIO.HIGH)
@@ -44,8 +46,8 @@ class TMC2130():
         self.spi.close()
         GPIO.cleanup()
 
-    def Write(self,Cmd,DataIn):
-        Data = np.uint32(DataIn)
+    def Write(self,Cmd,DatatoSend):
+        Data = np.uint32(DatatoSend)
 
         Data_byte1 = int((Data >> 24) & 0xFF)
         Data_byte2 = int((Data >> 16) & 0xFF)
@@ -56,20 +58,74 @@ class TMC2130():
         data1 = [Cmd,Data_byte1,Data_byte2,Data_byte3,Data_byte4]
 
         #Transmit the data via spi bus
-        GPIO.output(self.CS_Pin, GPIO.LOW)
         self.spi.xfer3(data1)
-        GPIO.output(self.CS_Pin, GPIO.HIGH)
 
+
+    def Read(self,Cmd):
+
+        ReadCmd = [Cmd,0x00,0x00,0x00,0x00]
+        self.spi.xfer3(ReadCmd)
+        Data_read = self.spi.readbytes(4)
+
+
+        return Data_read
+
+    def Enable(self):
+        GPIO.output(self.En_Pin, GPIO.LOW)
+
+    def Disable(self):
+        GPIO.output(self.En_Pin, GPIO.HIGH)
+
+    def OneStep(self):
+        GPIO.output(self.Step_Pin, GPIO.HIGH)
+        time.sleep(1/1000000)
+        GPIO.output(self.Step_Pin, GPIO.LOW)
+        time.sleep(1 / 1000000)
+
+    def Shaft (self):
+        GPIO.output(self.Dir_Pin, not GPIO.input(Dir_Pin))
+        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_GCONF, 1<<4 | 0x00000001)
 
 if __name__ == "__main__":
+    def ChangeDir():
+        MyTMC2130.Shaft()
+        print ("Hello\n")
+
     import time
+    from threading import Timer
     En_Pin = 31
     Dir_Pin = 32
     Step_Pin = 33
-    CS_Pin = 36
-    MyTMC2130 = TMC2130(En_Pin,Dir_Pin,Step_Pin,CS_Pin)
+    CS_Pin = 15
+    try:
 
-    while True:
-        TestData = 0x85154855
-        MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_GCONF,TestData)
+        MyTMC2130 = TMC2130(En_Pin,Dir_Pin,Step_Pin,CS_Pin)
+        MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_GCONF, 0x00000001) # voltage on AIN is current reference
+        MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_IHOLD_IRUN, 0x00001010) #IHOLD=0x10, IRUN=0x10
+        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x00008008)  # 256 microstep, MRERS=1=24, TBL=1=24,TOFF=8
+        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x01008008)  # 128 microstep, MRERS=1=24, TBL=1=24,TOFF=8
+        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x02008008)  # 64  microstep, MRERS=1=24, TBL=1=24,TOFF=8
+        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x03008008)  # 32  microstep, MRERS=1=24, TBL=1=24,TOFF=8
+        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x04008008)  # 16  microstep, MRERS=1=24, TBL=1=24,TOFF=8
+        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x05008008)  # 08  microstep, MRERS=1=24, TBL=1=24,TOFF=8
+        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x06008008)  # 04  microstep, MRERS=1=24, TBL=1=24,TOFF=8
+        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x07008008)  # 02 microstep, MRERS=1=24, TBL=1=24,TOFF=8
+        MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x08008008)  # 01 microstep, MRERS=1=24, TBL=1=24,TOFF=8
+
+        time.sleep(0.1)
+        MyTMC2130.Enable()
+        t1 = Timer(5,ChangeDir)
+        t2 = Timer(8, ChangeDir)
+        t1.start()
+        t2.start()
+        while True:
+            MyTMC2130.OneStep()
+            time.sleep(0.001)
+
+
+    finally:
+        MyTMC2130.Disable()
+        MyTMC2130.__del__()
+        pass
+
 
