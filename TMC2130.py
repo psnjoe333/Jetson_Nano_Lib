3b52c4f60a57756fd380de5dc4701b34269c0be7
psnjoe333 <liwanjoe@gmail.com>
2021-04-04 04:24:45 +0800

    Able to wrtie and read via SPI

--- TMC2130.py
+++ TMC2130.py
@@ -1,3 +1,7 @@
+# Author : Liwan, Huang
+# Date : April, 2021
+# This program must wire the 4 lines of spi2 (/dev/SPI1.1)
+
 import spidev
 import Jetson.GPIO as GPIO
 import numpy as np
@@ -5,7 +9,7 @@ import time
 
 class TMC2130():
 
-    def __init__(self, En_PinIn, Dir_PinIn, Step_PinIn,CS_PinIn,busIn=0, deviceIn=0):
+    def __init__(self, En_PinIn, Dir_PinIn, Step_PinIn, busIn=0, deviceIn=0):
 
         # Import pin and bus setup from input
         self.En_Pin = En_PinIn
@@ -13,19 +17,23 @@ class TMC2130():
         self.Step_Pin = Step_PinIn
         self.bus = busIn
         self.device = deviceIn
-        self.CS_Pin = CS_PinIn
 
         #Define the register address
         self.WriteFlag      =        (1<<7)
         self.ReadFlag       =        (0<<7)
         self.Reg_GCONF      =        0x00
         self.Reg_GSTAT      =        0x01
+        self.Reg_IOIN       =        0x04
         self.Reg_IHOLD_IRUN =        0x10
+        self.Reg_TPOWERDOWN =        0x11
+        self.Reg_TSTEP      =        0x12
+        self.Reg_TRWMTHRS   =        0x13
         self.Reg_CHOPCONF   =        0x6C
         self.Reg_DCCTRL     =        0x6D
         self.Reg_DRVSTAT    =        0x6F
+        self.Reg_PWMCONF    =        0x70
 
-        # Implement spi device object
+        #Implement and set up he spi device object
         self.spi = spidev.SpiDev()
         self.spi.open(self.bus, self.device)  # SPI2---->MOSI:37pin (/dev/SPI1.1)
         self.spi.max_speed_hz = 1000000
@@ -39,12 +47,13 @@ class TMC2130():
         GPIO.setup(self.En_Pin, GPIO.OUT, initial=GPIO.HIGH) #LOW -> active
         GPIO.setup(self.Dir_Pin, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.Step_Pin, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(self.CS_Pin, GPIO.OUT, initial=GPIO.HIGH)
 
 
     def __del__(self):
-        self.spi.close()
+        self.Disable()
         GPIO.cleanup()
+        self.spi.close()
+
 
     def Write(self,Cmd,DatatoSend):
         Data = np.uint32(DatatoSend)
@@ -62,29 +71,32 @@ class TMC2130():
 
 
     def Read(self,Cmd):
-
-        ReadCmd = [Cmd,0x00,0x00,0x00,0x00]
-        self.spi.xfer3(ReadCmd)
-        Data_read = self.spi.readbytes(4)
-
-
-        return Data_read
+        #Read 4 bytes data via spi
+        Data = 0
+        ReadCmd = [Cmd,0x00,0x00,0x00,0x00] #sen Read command and register to read to TMC2130
+        Data_read = self.spi.xfer3(ReadCmd)
+        Data_read = list(Data_read)
+        Data |= ( Data_read[0]<<32 |  Data_read[1]<<24 | Data_read[2] <<16 | Data_read[3] <<  8 | Data_read[4])
+        return Data
 
     def Enable(self):
         GPIO.output(self.En_Pin, GPIO.LOW)
+        print("Enable!")
 
     def Disable(self):
         GPIO.output(self.En_Pin, GPIO.HIGH)
+        print("Disable!")
 
     def OneStep(self):
         GPIO.output(self.Step_Pin, GPIO.HIGH)
-        time.sleep(1/1000000)
+        time.sleep(10 /1000000)
         GPIO.output(self.Step_Pin, GPIO.LOW)
-        time.sleep(1 / 1000000)
 
     def Shaft (self):
         GPIO.output(self.Dir_Pin, not GPIO.input(Dir_Pin))
-        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_GCONF, 1<<4 | 0x00000001)
+        #Shaft_Status = (0x01) & (self.Read(MyTMC2130.Reg_GCONF)>>4)
+        #print(Shaft_Status)
+        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_GCONF, ((~bool(Shaft_Status))<<4 | 0x00000001))
 
 if __name__ == "__main__":
     def ChangeDir():
@@ -96,35 +108,36 @@ if __name__ == "__main__":
     En_Pin = 31
     Dir_Pin = 32
     Step_Pin = 33
-    CS_Pin = 15
     try:
+        # Create the TMC2130 object
+        MyTMC2130 = TMC2130(En_Pin,Dir_Pin,Step_Pin)
 
-        MyTMC2130 = TMC2130(En_Pin,Dir_Pin,Step_Pin,CS_Pin)
-        MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_GCONF, 0x00000001) # voltage on AIN is current reference
-        MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_IHOLD_IRUN, 0x00001010) #IHOLD=0x10, IRUN=0x10
-        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x00008008)  # 256 microstep, MRERS=1=24, TBL=1=24,TOFF=8
-        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x01008008)  # 128 microstep, MRERS=1=24, TBL=1=24,TOFF=8
-        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x02008008)  # 64  microstep, MRERS=1=24, TBL=1=24,TOFF=8
-        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x03008008)  # 32  microstep, MRERS=1=24, TBL=1=24,TOFF=8
-        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x04008008)  # 16  microstep, MRERS=1=24, TBL=1=24,TOFF=8
-        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x05008008)  # 08  microstep, MRERS=1=24, TBL=1=24,TOFF=8
-        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x06008008)  # 04  microstep, MRERS=1=24, TBL=1=24,TOFF=8
-        #MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x07008008)  # 02 microstep, MRERS=1=24, TBL=1=24,TOFF=8
-        MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF, 0x08008008)  # 01 microstep, MRERS=1=24, TBL=1=24,TOFF=8
+        # Set up the config of TMC2130
+        MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_GCONF       , 0x00000005)   # EN_PWM_MODE=1, Voltage on AIN is current reference
+        MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_IHOLD_IRUN  , 0x00001010)   # IHOLD=0x10, IRUN=0x10
+        MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_CHOPCONF    , 0x080100C3)   # CHOPCONF: MicroStep: full, TOFF=3, HSTRT=4,HEND=2,TBL=0(speadCycle)
+        MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_TPOWERDOWN  , 0x0000000A)   # TPOWERDOWN=10 #Delay before power down in stand still
+        MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_TRWMTHRS    , 0x000001F4)   # TPWM_THRS=500 yields a switching velocity about 3500 = ca. 30RPM
+        MyTMC2130.Write(MyTMC2130.WriteFlag | MyTMC2130.Reg_PWMCONF     , 0x000401C8)   # PWM_CONF: AUTO=1, 2/1024 Fclk, Switch amplitude limit = 200, Grad=1
 
         time.sleep(0.1)
         MyTMC2130.Enable()
+        time.sleep(0.1)
         t1 = Timer(5,ChangeDir)
-        t2 = Timer(8, ChangeDir)
         t1.start()
+        t2 = Timer(15,ChangeDir)
         t2.start()
+
         while True:
+            #data = MyTMC2130.Read(MyTMC2130.Reg_CHOPCONF)
+            #print (hex(data))
             MyTMC2130.OneStep()
             time.sleep(0.001)
 
+    except KeyboardInterrupt:
+        print("Exiting Program")
 
     finally:
-        MyTMC2130.Disable()
         MyTMC2130.__del__()
         pass
 
